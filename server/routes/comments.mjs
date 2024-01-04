2e453ef78d7f72b816c9f38b1ad0f36ac70083be
Natalia <savenkon@gmail.com>
2024-01-04 23:28:12 +0200

    1

--- server/routes/comments.mjs
+++ server/routes/comments.mjs
@@ -6,7 +6,9 @@ import CommentController from '../controllers/CommentController.mjs'
 
 router.get('/comment',CommentController.all);
 
+
 router.get('/comment/:id',CommentController.getById);
+
 
 
 export default router;
